1e174d7390395cee02cbf0f9a005ecd83e3b05e7
poziworld <poziworld@gmail.com>
2016-01-23 20:44:53 -0800

    Minor refactoring of message-handler.js
    Незначительный рефакторинг message-handler.js

--- message/message-handler.js
+++ message/message-handler.js
@@ -1,15 +1,17 @@
 ( function() {
-  var
-      strQueryString  = location.search
-    , strIndicator    = '?lang='
+  'use strict';
+
+  // Check whether there is a message in the requested language
+  var strQueryString = location.search
+    , strIndicator = '?lang='
     ;
 
+  // Check whether the language is specified
   if ( strQueryString.indexOf( strIndicator ) === 0 && strQueryString.length > 7 ) {
-    var
-        strLang   = strQueryString
+    var strLang = strQueryString
                       .replace( strIndicator, '' )
                       .substr( 0, 2 )
-      , $message  = document.getElementById( strLang )
+      , $message = document.getElementById( strLang )
       ;
 
     if ( document.contains( $message ) ) {
@@ -24,9 +26,7 @@
   }
 
   function showMessage( $message ) {
-    var strTitle = $message.getAttribute( 'data-title' );
-
-    document.title = strTitle;
+    document.title = $message.getAttribute( 'data-title' );
 
     $message.style.display = 'block';
     $message.setAttribute( 'hidden', false );
